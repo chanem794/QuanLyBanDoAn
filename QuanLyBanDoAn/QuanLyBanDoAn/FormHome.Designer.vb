463841e6f0516f03e51d19b2ae8671d6e6435e3d
Dinh <149035922+chanem794@users.noreply.github.com>
2025-05-20 20:42:44 +0700

    Done Gọi Bàn

--- QuanLyBanDoAn/QuanLyBanDoAn/FormHome.Designer.vb
+++ QuanLyBanDoAn/QuanLyBanDoAn/FormHome.Designer.vb
@@ -22,25 +22,51 @@ Partial Class FormHome
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.flpBan = New System.Windows.Forms.FlowLayoutPanel()
+        Me.btnKinhDoanh = New System.Windows.Forms.Button()
+        Me.btnThongKe = New System.Windows.Forms.Button()
+        Me.PanelHome = New System.Windows.Forms.Panel()
         Me.SuspendLayout()
         '
-        'flpBan
+        'btnKinhDoanh
         '
-        Me.flpBan.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
-            Or System.Windows.Forms.AnchorStyles.Left) _
-            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.flpBan.Location = New System.Drawing.Point(12, 68)
-        Me.flpBan.Name = "flpBan"
-        Me.flpBan.Size = New System.Drawing.Size(776, 370)
-        Me.flpBan.TabIndex = 0
+        Me.btnKinhDoanh.BackColor = System.Drawing.Color.FromArgb(CType(CType(39, Byte), Integer), CType(CType(67, Byte), Integer), CType(CType(129, Byte), Integer))
+        Me.btnKinhDoanh.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnKinhDoanh.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.btnKinhDoanh.Location = New System.Drawing.Point(12, 12)
+        Me.btnKinhDoanh.Name = "btnKinhDoanh"
+        Me.btnKinhDoanh.Size = New System.Drawing.Size(225, 45)
+        Me.btnKinhDoanh.TabIndex = 1
+        Me.btnKinhDoanh.Text = "Kinh Doanh"
+        Me.btnKinhDoanh.UseVisualStyleBackColor = False
+        '
+        'btnThongKe
+        '
+        Me.btnThongKe.BackColor = System.Drawing.Color.Silver
+        Me.btnThongKe.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnThongKe.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.btnThongKe.Location = New System.Drawing.Point(243, 12)
+        Me.btnThongKe.Name = "btnThongKe"
+        Me.btnThongKe.Size = New System.Drawing.Size(225, 45)
+        Me.btnThongKe.TabIndex = 2
+        Me.btnThongKe.Text = "Thống Kê"
+        Me.btnThongKe.UseVisualStyleBackColor = False
+        '
+        'PanelHome
+        '
+        Me.PanelHome.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.PanelHome.Location = New System.Drawing.Point(0, 80)
+        Me.PanelHome.Name = "PanelHome"
+        Me.PanelHome.Size = New System.Drawing.Size(800, 370)
+        Me.PanelHome.TabIndex = 3
         '
         'FormHome
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.flpBan)
+        Me.Controls.Add(Me.PanelHome)
+        Me.Controls.Add(Me.btnThongKe)
+        Me.Controls.Add(Me.btnKinhDoanh)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
         Me.Name = "FormHome"
         Me.Text = "FormHome"
@@ -48,6 +74,7 @@ Partial Class FormHome
         Me.ResumeLayout(False)
 
     End Sub
-
-    Friend WithEvents flpBan As FlowLayoutPanel
+    Friend WithEvents btnKinhDoanh As Button
+    Friend WithEvents btnThongKe As Button
+    Friend WithEvents PanelHome As Panel
 End Class
